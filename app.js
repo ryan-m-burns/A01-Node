62faffa5892bc62f57a0c269c71c7e55c14a9e41
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2025-02-22 11:33:46 -0800

    add styles folder, start setting up app

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const morgan = require('morgan');
+const logger = require('morgan');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Use morgan to log requests
-app.use(morgan('dev'));
+// Use logger as middleware, with 3 different output templates
+app.use(logger('dev'));
+app.use(logger('common'));
+app.use(logger('combined'));
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
@@ -13,6 +15,18 @@ app.use(express.static('public'));
 // Define a simple route
 app.get('/', (req, res) => {
     res.send('Hello, World!');
+});
+
+
+
+
+
+
+
+
+
+app.all('*', (req, res) => {
+    res.status(404).send('Not Found');
 });
 
 // Start the server
