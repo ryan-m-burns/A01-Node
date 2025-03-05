e85935dc0445f5cbe0e36af0e6834764aa311a91
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2025-03-04 16:01:38 -0800

    adjust search functionality

--- data/projectOps.js
+++ data/projectOps.js
@@ -12,6 +12,20 @@ class ProjectOps {
         console.log(`Getting project with id ${id}...`);
         return await Project.findById(id);
     }
+
+    static async searchProjects(query) {
+        console.log(`Searching projects with query: ${query}`);
+        const queryRegex = new RegExp(query, "i");
+
+        return await Project.find({
+            $or: [
+                { title: queryRegex },
+                { summary: queryRegex },
+                { description: queryRegex },
+                { tech: queryRegex },
+            ],
+        });
+    }
 }
 
 module.exports = ProjectOps;
