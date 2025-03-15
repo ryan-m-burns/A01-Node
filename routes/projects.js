c91535ada83f54ae34667e74c9b529869bc52ce5
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2025-03-15 16:25:24 -0700

    add create, update, delete to project routes

--- routes/projects.js
+++ routes/projects.js
@@ -1,14 +1,26 @@
-const ProjectController = require("../controllers/ProjectController");
-const express = require("express");
+const ProjectController = require('../controllers/ProjectController');
+const express = require('express');
 const router = express.Router();
 
 // Get all projects
-router.get("/", ProjectController.Index);
+router.get('/', ProjectController.Index);
 
 // Search projects
-router.get("/search", ProjectController.Search);
+router.get('/search', ProjectController.Search);
+
+// Create new project
+router.get('/create', ProjectController.Create);
+router.post('/create', ProjectController.Create);
+
+// Update project
+router.get('/update/:id', ProjectController.Update);
+router.post('/update/:id', ProjectController.Update);
+
+// Delete project
+router.get('/delete/:id', ProjectController.Delete);
+router.post('/delete/:id', ProjectController.Delete);
 
 // Get project by ID (must be last to avoid conflicts with other routes)
-router.get("/:id", ProjectController.Detail);
+router.get('/:id', ProjectController.Detail);
 
 module.exports = router;
