26317729e7be6b2964955a3130bebd9bddd50046
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2025-04-05 17:34:35 -0700

    add User model

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+// User Schema
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+});
+// Add passport-local-mongoose to our Schema
+userSchema.plugin(passportLocalMongoose);
+
+// Pass the Schema into Mongoose to use as our model
+const User = mongoose.model('User', userSchema);
+
+// Export it so that we can use the model in our App
+module.exports = User;
